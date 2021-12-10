e6deadb32ab870e78b539415242b883480a0878e
lud0v1c <eltonicha@outlook.com>
2021-12-10 17:44:47 +0000

    Add default loki chart

--- helm.tf
+++ helm.tf
@@ -7,3 +7,13 @@ resource "helm_release" "grafana" {
     templatefile("charts/grafana/values.yaml", { adminUser = "${var.grafana_admin_user}", adminPassword = "${var.grafana_admin_password}" })
   ]
 }
+
+resource "helm_release" "loki" {
+  name      = "loki"
+  chart     = "charts/loki"
+  namespace = "logging"
+
+  depends_on = [
+    helm_release.grafana
+  ]
+}
